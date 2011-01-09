6c745fd9a284f4a278e030342334e0c1bb98f9fd
Brandon Arbini <brandon@zencoder.com>
2011-01-09 00:45:09 -0600

    Ensuring EXTINF always has a comma.

--- lib/m3uzi/file.rb
+++ lib/m3uzi/file.rb
@@ -1,15 +1,11 @@
 class M3Uzi
-
   class File
 
     attr_accessor :path, :duration, :description
 
     def attribute_string
-      s = []
-      s << duration
-      s << description if description
-      s.join(',')
+      "#{duration},#{description}"
     end
-  end
 
+  end
 end
